5abbca9a4f8486a20b45c07fac33c008c52182f4
mrbalpande21 <116449729+mrbalpande21@users.noreply.github.com>
2022-10-30 00:47:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 import os
-PORT_NUMBER = 5005
+# PORT_NUMBER = 5005
 
 MODEL_FILE_PATH = os.path.join("Project_app", "logistic_regression.pkl")
 JSON_FILE_PATH = os.path.join("Project_app", "project_data.json")
